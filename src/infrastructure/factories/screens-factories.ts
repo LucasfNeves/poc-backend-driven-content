0ab59febaad778c1cf0d8724c474006c7cefe714
LucasfNeves <lucasfarias.ln@gmail.com>
2026-01-27 13:32:31 -0300

    feat: adicionar controlador SaveScreen e rotas para salvar telas

--- src/infrastructure/factories/screens-factories.ts
+++ src/infrastructure/factories/screens-factories.ts
@@ -1,11 +1,21 @@
 import { GetScreenByIdController } from '@/application/controller/GetScreenByIdController';
+import { SaveScreenController } from '@/application/controller/SaveScreenController';
 import { GetScreenByIdUseCase } from '@/application/use-cases/GetScreenByIdUseCase';
+import { SaveScreenUseCase } from '@/application/use-cases/SaveScreenUseCase';
 import { ScreenRepository } from '@/infrastructure/repositories/postgres/ScreenRepository';
 
 export const makeGetScreenByIdController = (): GetScreenByIdController => {
   const screenRepository = new ScreenRepository();
   const getScreenByIdUseCase = new GetScreenByIdUseCase(screenRepository);
   const controller = new GetScreenByIdController(getScreenByIdUseCase);
+
+  return controller;
+};
+
+export const makeSaveScreenController = (): SaveScreenController => {
+  const screenRepository = new ScreenRepository();
+  const saveScreenUseCase = new SaveScreenUseCase(screenRepository);
+  const controller = new SaveScreenController(saveScreenUseCase);
 
   return controller;
 };
