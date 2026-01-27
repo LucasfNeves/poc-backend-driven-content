9803db4117f9b9bf8c0afcbb6947266d7ee51b4d
LucasfNeves <lucasfarias.ln@gmail.com>
2026-01-27 11:00:19 -0300

    fix: atualizar a configuração do Prisma para usar o adaptador PostgreSQL

--- src/infrastructure/database/prisma/client.ts
+++ src/infrastructure/database/prisma/client.ts
@@ -1,17 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../../generated/prisma/client.js';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
-};
+const connectionString = `${process.env.DATABASE_URL}`;
 
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prismaGlobal = prisma;
-}
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
