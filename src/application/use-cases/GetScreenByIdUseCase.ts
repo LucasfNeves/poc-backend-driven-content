c2b7833ad4c48d5c67a88720118cc62565c8f9e7
LucasfNeves <lucasfarias.ln@gmail.com>
2026-01-28 12:02:22 -0300

    refactor: remover validação de UUID do ID da tela na controladora GetScreenById

--- src/application/use-cases/GetScreenByIdUseCase.ts
+++ src/application/use-cases/GetScreenByIdUseCase.ts
@@ -1,14 +1,11 @@
 import { Screen } from '@/domain/entities/Screen';
 import { IScreenRepository } from '@/domain/interfaces/IScreenRepository';
-import { ValidationHelper } from '@/shared/helpers/ValidationHelper';
 import { NotFoundError } from '@/shared/errors/AppErrors';
 
 export class GetScreenByIdUseCase {
   constructor(private readonly screenRepository: IScreenRepository) {}
 
   async execute(id: string): Promise<Screen> {
-    ValidationHelper.validateUUID(id, 'Screen ID');
-
     const screen = await this.screenRepository.findById(id);
 
     if (!screen) {
