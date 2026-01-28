5082e4e96175b7903c423f6b206ebd662f46e27b
LucasfNeves <lucasfarias.ln@gmail.com>
2026-01-28 15:49:18 -0300

    refactor: remover controladores e casos de uso relacionados a telas

--- src/domain/schemas/validateIdSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const validateIdSchema = z.object({
-  id: z.uuid({ error: 'id must be a valid UUID' }),
-});
-
-export type GetScreenByIdParamsDTO = z.infer<typeof validateIdSchema>;
