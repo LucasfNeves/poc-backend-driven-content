f43c9d1aafc93475de1447490a293f1a64b5af1b
LucasfNeves <lucasfarias.ln@gmail.com>
2026-01-29 11:11:05 -0300

    feat: ajustar esquema de validação para permitir campos opcionais em getComponentsRequest

--- src/application/controller/schemas/getComponentsRequestSchema.ts
+++ src/application/controller/schemas/getComponentsRequestSchema.ts
@@ -2,10 +2,12 @@ import z from 'zod';
 import { uuidSchema } from './common';
 
 export const getComponentsRequestSchema = z.object({
-  query: z.object({
-    id: uuidSchema.optional(),
-    name: z.string().optional(),
-  }),
+  query: z
+    .object({
+      id: uuidSchema,
+      name: z.string(),
+    })
+    .partial(),
 });
 
 export type GetComponentsRequest = z.infer<typeof getComponentsRequestSchema>;
