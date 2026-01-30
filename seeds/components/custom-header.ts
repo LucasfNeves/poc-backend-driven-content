66ebc7cf8fdf1651e31ae4eed5acaa4fe82fd148
LucasfNeves <lucasfarias.ln@gmail.com>
2026-01-30 09:42:59 -0300

    feat: refatorar customHeader para usar AppBarBuilder com novas propriedades

--- seeds/components/custom-header.ts
+++ seeds/components/custom-header.ts
@@ -1,10 +1,30 @@
-import { CustomHeaderBuilder } from '@/domain/components/builders/CustomHeaderBuilder';
-import { defaultBuilderFactory } from '@/domain/components/builders/BuilderFactory';
+import { AppBarBuilder } from '@/domain/components/builders/AppBarBuilder';
+import { ImageBuilder } from '@/domain/components/builders/ImageBuilder';
+import { IconBuilder } from '@/domain/components/builders/IconBuilder';
+import { IconButtonBuilder } from '@/domain/components/builders/IconButtonBuilder';
 import { DesignTokens } from '@/domain/components/constants/DesignTokens';
 
-export const customHeader = new CustomHeaderBuilder(defaultBuilderFactory)
-  .backgroundColor(DesignTokens.colors.background)
-  .foregroundColor(DesignTokens.colors.text)
-  .title('MOBi', DesignTokens.fontSize.xlarge)
-  .leadingIcon('menu')
-  .build();
+export const customHeader = AppBarBuilder.create({
+  backgroundColor: DesignTokens.colors.black,
+  foregroundColor: DesignTokens.colors.black,
+  centerTitle: true,
+  elevation: DesignTokens.elevation.none,
+  title: ImageBuilder.create('assets/logo.svg', {
+    height: 40,
+    fit: 'contain',
+  }),
+  leading: IconButtonBuilder.create(
+    IconBuilder.create('menu', {
+      color: DesignTokens.colors.black,
+      size: DesignTokens.iconSize.medium,
+    }),
+  ),
+  actions: [
+    IconButtonBuilder.create(
+      IconBuilder.create('search', {
+        color: DesignTokens.colors.black,
+        size: DesignTokens.iconSize.medium,
+      }),
+    ),
+  ],
+});
