f63bfc48ddb256cb97058f75f185d85c134ea001
LucasfNeves <lucasfarias.ln@gmail.com>
2026-02-03 11:17:27 -0300

    feat: adicionar design tokens e atualizar importações de schemas nos controladores

---
+++ seeds/tokens/design-tokens.ts
@@ -0,0 +1,29 @@
+export const tokens = {
+  colors: {
+    white: 0xffffffff,
+    darkGray: 0xff333333,
+    primary: 0xff2196f3,
+    secondary: 0xffff5722,
+  },
+  fontSize: {
+    small: 12,
+    medium: 16,
+    large: 20,
+    xlarge: 24,
+  },
+  iconSize: {
+    small: 16,
+    medium: 20,
+    large: 24,
+  },
+  elevation: {
+    none: 0,
+    low: 2,
+    medium: 4,
+    high: 8,
+  },
+  fontWeight: {
+    normal: 'w400',
+    bold: 'w700',
+  },
+} as const;
